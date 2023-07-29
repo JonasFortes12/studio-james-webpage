0e0d9f5a5eb0615ed702b8dbfe231033fda912e3
JonasFortes12 <jonascforte@hotmail.com>
2023-07-29 19:45:45 -0300

    feat: Add carousel section
    In this point, miss adjuste the size font and button to mobile layout

---
+++ src/components/CarouselMod/index.jsx
@@ -0,0 +1,65 @@
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import { BsWhatsapp } from "react-icons/bs";
+// Importing the Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import style from "./CarouselMod.module.scss";
+
+import image1 from "../../assets/images/studio1.png";
+import image2 from "../../assets/images/studio2.png";
+import image3 from "../../assets/images/studio3.png";
+
+function CarouselMod() {
+  return (
+    <Carousel indicators={false} controls={true} className={style.carousel}>
+      
+      <Carousel.Item className={style.carousel__item}>
+        <div className={style.carousel__item__imgContainer}>
+          <img src={image1} alt="" />
+          <div></div>
+        </div>
+        <Carousel.Caption>
+          <h2>Conforto e beleza em sintonia</h2>
+          <p>
+            Ambiente acolhedor e confortável, onde você possa se sentir à
+            vontade para expressar suas preferências e desejos. Cada serviço é
+            realizado com um toque de elegância e precisão
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item className={style.carousel__item}>
+        <div className={style.carousel__item__imgContainer}>
+          <img src={image2} alt="" />
+          <div></div>
+        </div>
+        <Carousel.Caption>
+          <h2>Sorrisos e estilos deslumbrantes</h2>
+          <p>
+            Nosso compromisso com a excelência e o profissionalismo é refletido
+            em cada sorriso deslumbrante que deixamos em nossos clientes
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item className={style.carousel__item}>
+        
+        <div className={style.carousel__item__imgContainer}>
+          <img src={image3} alt="" />
+          <div></div>
+        </div>
+        
+        <Carousel.Caption>
+          <h2>Inovação e qualidade em um só lugar.</h2>
+          <Button href="https://www.seusite.com" className={style.carousel__button}>
+            <BsWhatsapp style={{marginRight: 10, marginBottom: 3}} size={20}/>
+            Mande uma mensagem!
+          </Button>{" "}
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselMod;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,11 @@
+import CarouselMod from "../../components/CarouselMod";
 import NavBar from "../../components/NavBar";
 
 function HomePage() {
   return (
     <>
       <NavBar/>
+      <CarouselMod/>
     </>
   );
 }
