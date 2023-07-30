bcfe8f8b4c1752709556797b9326f5276d55e3e7
JonasFortes12 <jonascforte@hotmail.com>
2023-07-30 19:00:18 -0300

    feat: Create a element for comport the sections contents

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,23 @@
+import style from './HomePage.module.scss';
+
 import CarouselMod from "../../components/CarouselMod";
 import NavBar from "../../components/NavBar";
+import PageSection from '../../components/PageSection';
+import { width } from '@mui/system';
+import { red } from '@mui/material/colors';
 
 function HomePage() {
   return (
     <>
       <NavBar/>
       <CarouselMod/>
+    
+      <main className={style.mainSection}>
+        <PageSection title={'CONHEÇA'} />
+        <PageSection title={'SERVIÇOS'} />
+        <PageSection title={'RESULTADOS'} />
+      </main>
+
     </>
   );
 }
