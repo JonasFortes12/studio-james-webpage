fb0a8fb7e5be3e1a219000ab91e76d7ac2c0714d
JonasFortes12 <jonascforte@hotmail.com>
2023-08-19 18:01:37 -0300

    style: Add services images and descriptions on coverflow cards

--- src/components/Servic/index.jsx
+++ src/components/Servic/index.jsx
@@ -1,61 +1,105 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import './Servic.css';
+import "./Servic.css";
 
 // import required modules
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
-
-import slide_img_1 from '../../assets/images/imgServices/img_1.jpg'
-import slide_img_2 from '../../assets/images/imgServices/img_2.jpg'
-import slide_img_3 from '../../assets/images/imgServices/img_3.jpg'
-import slide_img_4 from '../../assets/images/imgServices/img_4.jpg'
-
+import makeup from '../../assets/images/imgServices/makeup_service.png'
+import hearStyle from '../../assets/images/imgServices/penteado_service.png'
+import locks from '../../assets/images/imgServices/mechas_service.png'
+import hairCutting from '../../assets/images/imgServices/corte_service.png'
+import sealing from '../../assets/images/imgServices/selagem_sevice.png'
+import eyebrows from '../../assets/images/imgServices/sobrancelha_service.png'
+import nails from '../../assets/images/imgServices/unhas_service.png'
 
 function Servic() {
   return (
     <div className="container">
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
+        loop={false}
+        slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
           depth: 100,
           modifier: 2.5,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
       >
         <SwiperSlide>
-          <img src={slide_img_1} alt="slide_img" />
+          <div className="slide-content">
+            <img src={makeup} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Maquiagens</h3>
+          </div>
         </SwiperSlide>
+
         <SwiperSlide>
-          <img src={slide_img_2} alt="slide_img" />
+          <div className="slide-content">
+            <img src={hearStyle} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Penteados</h3>
+          </div>
         </SwiperSlide>
+
         <SwiperSlide>
-          <img src={slide_img_3} alt="slide_img" />
+          <div className="slide-content">
+            <img src={locks} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Mechas</h3>
+          </div>
         </SwiperSlide>
+
         <SwiperSlide>
-          <img src={slide_img_4} alt="slide_img" />
+          <div className="slide-content">
+            <img src={hairCutting} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Cortes</h3>
+          </div>
         </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="slide-content">
+            <img src={sealing} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Selagem</h3>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="slide-content">
+            <img src={eyebrows} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Sobrancelhas</h3>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="slide-content">
+            <img src={nails} alt="slide_img" />
+            <div class="gradient"></div>
+            <h3 className="text">Unhas</h3>
+          </div>
+        </SwiperSlide>
+
 
         {/* <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
